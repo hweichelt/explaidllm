5c45788cf633da7aa8693cf79e313ac185d90901
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-07-01 14:54:16 +0200

    Added logging

--- src/explaidllm/__main__.py
+++ src/explaidllm/__main__.py
@@ -1,11 +1,16 @@
+import logging
 import sys
 
 from clingo.application import clingo_main
 
 from .cli import ExplaidLlmApp
 
+logger = logging.getLogger(__name__)
+
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+    logger.info("Starting ExplaidLLM")
     clingo_main(ExplaidLlmApp(sys.argv[0]), sys.argv[1:] + ["-V0"])
 
 
