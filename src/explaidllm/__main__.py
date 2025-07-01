9765819c0e2061ebb510fa956f1584b4e3c05309
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-07-01 14:43:13 +0200

    Added ExplaidLLMApplication to __main__

--- src/explaidllm/__main__.py
+++ src/explaidllm/__main__.py
@@ -1,5 +1,12 @@
+import sys
+
+from clingo.application import clingo_main
+
+from .cli import ExplaidLlmApp
+
+
 def main():
-    print("Hello ExplaidLLM!")
+    clingo_main(ExplaidLlmApp(sys.argv[0]), sys.argv[1:] + ["-V0"])
 
 
 if __name__ == "__main__":
