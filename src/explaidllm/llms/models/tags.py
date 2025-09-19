e8a22c8222dfe0ef311ed5a8d0101f804a281ce6
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-09-19 12:45:39 +0200

    Added GPT-5 models

--- src/explaidllm/llms/models/tags.py
+++ src/explaidllm/llms/models/tags.py
@@ -15,5 +15,8 @@ class Tag:
 class ModelTag(Enum):
     """Language model specifier"""
 
+    GPT_5 = Tag(openai="gpt-5")
+    GPT_5_MINI = Tag(openai="gpt-5-mini")
+    GPT_5_NANO = Tag(openai="gpt-5-nano")
     GPT_4O = Tag(openai="gpt-4o")
     GPT_4O_MINI = Tag(openai="gpt-4o-mini")
